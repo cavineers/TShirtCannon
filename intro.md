2de7bb0421dddba0bd65b511c67ebf9332c7893c
Bryce Cary <bcary.md@gmail.com>
2020-03-21 14:52:10 -0400

    Removed an unused import

---
+++ intro.md
@@ -0,0 +1,15 @@
+Hi guys, my name is Bryce and i'm the software lead for team 4541
+
+Im going to making videos about FRC programming, everything from coding a robot to building a custom dashboard for your team.
+
+In this video today, we'll be focussing on getting your environment setup for coding a robot.
+
+So, let's get started
+
+---
+
+Hi guys, my name is Bryce and i'm the software lead for team 4541
+
+In this video today, we'll be talking about the basic functionality of the command based system and utilizing the libraries we installed in the last video
+
+So, let's get started
--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -3,7 +3,6 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
-import frc.robot.RobotContainer.SafetyMode;
 import frc.robot.subsystems.Cannon;
 
 public class Shoot extends CommandBase {
@@ -25,12 +24,12 @@ public class Shoot extends CommandBase {
     @Override
     public void initialize() {
         // If the safety is off
-        if (this.m_rc.controllerMode == SafetyMode.SAFETY_OFF) {
+        if (this.m_rc.controllerMode == RobotContainer.SafetyMode.SAFETY_OFF) {
             // Open the barrel
             this.m_cannon.open();
 
             // Turn the safety back on
-            this.m_rc.controllerMode = SafetyMode.SAFETY_ON;
+            this.m_rc.controllerMode = RobotContainer.SafetyMode.SAFETY_ON;
         }
 
         // Start the timer
