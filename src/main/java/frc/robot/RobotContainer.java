914643ac83e510356cb6cd881130854698ff7f63
Ryan Bode <51761134+Ryan4545@users.noreply.github.com>
2021-09-12 14:52:55 -0400

    feat: updated to new CAD design

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,8 +5,6 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
 import frc.robot.commands.SetSafety;
 import frc.robot.commands.Shoot;
-import frc.robot.commands.TogglePistons;
-import frc.robot.subsystems.SealingPiston;
 import frc.robot.subsystems.Cannon;
 import frc.robot.subsystems.DriveTrain;
 
@@ -25,23 +23,25 @@ public class RobotContainer {
   public JoystickButton m_leftStick = new JoystickButton(joy, 9);
   public JoystickButton m_rightStick = new JoystickButton(joy, 10);
 
-  public POVButton m_dPadLeft = new POVButton(this.joy, 270);
+  public POVButton m_dPadTop = new POVButton(this.joy, 0);
   public POVButton m_dPadRight = new POVButton(this.joy, 90);
+  public POVButton m_dPadBottom = new POVButton(this.joy, 180);
+  public POVButton m_dPadLeft = new POVButton(this.joy, 270);
 
-  // Controller map mode
+  // Safety Enum
   public enum SafetyMode {
     SAFETY_ON, // Can't shoot
     SAFETY_OFF // Can shoot
-  } 
+  }
 
   // Current mode
   public SafetyMode controllerMode = SafetyMode.SAFETY_ON;
 
   // Subsystems
   public DriveTrain drivetrain = new DriveTrain();
-  public SealingPiston pistons = new SealingPiston();
   public Cannon leftCannon = new Cannon(Constants.PCMChannels.kLeftCannon);
   public Cannon rightCannon = new Cannon(Constants.PCMChannels.kRightCannon);
+  public Cannon topCannon = new Cannon(Constants.PCMChannels.kTopCannon);
 
   public RobotContainer() {
     configureButtonBindings();
@@ -52,10 +52,8 @@ public class RobotContainer {
     m_rightMenu.whenPressed(new SetSafety(this, SafetyMode.SAFETY_OFF));
     m_leftMenu.whenPressed(new SetSafety(this, SafetyMode.SAFETY_ON));
 
-    // Toggle pistons
-    m_aButton.whenPressed(new TogglePistons(this.pistons));
-
     // D-pad buttons
+    m_dPadTop.whenPressed(new Shoot(this.topCannon, this));
     m_dPadLeft.whenPressed(new Shoot(this.leftCannon, this));
     m_dPadRight.whenPressed(new Shoot(this.rightCannon, this));
   }
